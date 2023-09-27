f1af99d7ba5754b802e5c7f5ac44d590535c4ad8
GoGalego <goncalo.galego@devscope.net>
2023-09-27 15:39:31 +0100

    Add Solution

---
+++ BlazorApp-TestShadow/wwwroot/js/shadow-dom.js
@@ -0,0 +1,30 @@
+﻿
+export function attachShadowDom(elementId) {
+    const host = document.getElementById(elementId);
+
+    if (!host.shadowRoot) {
+
+        const shadowRoot = host.attachShadow({ mode: "open" });
+
+        shadowRoot.innerHTML = `
+            <style>
+
+                * {
+                    font-size: 20px;
+                    background-color: lightyellow;
+                }
+
+                 .newDom{
+                    height:50px:
+                    width:100%;
+                }
+
+        
+            </style >
+            <div class="newDOM">
+                HTML
+            </div>
+        `;
+    }
+
+};
