8b37ecb0422bcc23fa51eeee9de8cfdc0be5314a
GoGalego <goncalo.galego@devscope.net>
2023-10-02 13:34:24 +0100

    Update

--- BlazorApp-TestShadow/wwwroot/js/shadow-dom.js
+++ BlazorApp-TestShadow/wwwroot/js/shadow-dom.js
@@ -1,30 +1,13 @@
 ﻿
-export function attachShadowDom(elementId) {
+export function attachShadowDom(elementId,previewHtml) {
     const host = document.getElementById(elementId);
 
     if (!host.shadowRoot) {
 
         const shadowRoot = host.attachShadow({ mode: "open" });
 
-        shadowRoot.innerHTML = `
-            <style>
-
-                * {
-                    font-size: 20px;
-                    background-color: lightyellow;
-                }
-
-                 .newDom{
-                    height:50px:
-                    width:100%;
-                }
-
-        
-            </style >
-            <div class="newDOM">
-                HTML
-            </div>
-        `;
+        var sourceDiv = document.getElementById("div-move");
+        shadowRoot.appendChild(sourceDiv);
     }
 
 };
