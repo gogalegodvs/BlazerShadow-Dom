9ea4a59bc6b0afb17e4748c569d6e87c69830f88
GoGalego <goncalo.galego@devscope.net>
2023-10-02 13:50:26 +0100

    Minor Fix Razor

--- BlazorApp-TestShadow/wwwroot/js/shadow-dom.js
+++ BlazorApp-TestShadow/wwwroot/js/shadow-dom.js
@@ -1,12 +1,12 @@
 ﻿
-export function attachShadowDom(elementId) {
+export function attachShadowDom(elementId,elementMove) {
     const host = document.getElementById(elementId);
 
     if (!host.shadowRoot) {
 
         const shadowRoot = host.attachShadow({ mode: "open" });
 
-        var sourceDiv = document.getElementById("div-move");
+        var sourceDiv = document.getElementById(elementMove);
         shadowRoot.appendChild(sourceDiv);
     }
 
